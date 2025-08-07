aaa2a35f8f47c5eafbb71c7fe496250cbad88e4d
Siddhant Mishra <siddhantmishra@Siddhants-MacBook-Pro.local>
2025-08-07 12:56:12 +0530

    Initial project setup

---
+++ deviceInfo/src/main/java/com/example/deviceInfo/TextPreview.kt
@@ -0,0 +1,11 @@
+package com.example.deviceInfo
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TextPreview(text: String) {
+    Text(
+        "Hello from library - $text"
+    )
+}
